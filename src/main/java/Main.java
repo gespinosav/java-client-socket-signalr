1e61ead81fcd8df7e14710ebfdd3779f4ce11d24
Gendrys Espinosa Vega <gendrys.espinosa14@gmail.com>
2019-02-28 14:20:06 -0500

    Add  params by console user and password

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,32 @@
 import com.microsoft.signalr.HubConnection;
 import com.microsoft.signalr.HubConnectionBuilder;
 
+import java.util.Base64;
+
 public class Main {
 
     public static void main(String[] args) {
         String url = "http://localhost:8088/sodexor";
+        String user = "usr";
+        String password = "password";
         if (args.length == 1) {
             url = args[0];
+            user = args[1];
+            password = args[2];
         }
 
+        String cred = user + ":" + password;
+
+        String resp = "Basic " + Base64.getEncoder().encode( cred.getBytes() );
+        String authParams = resp.replace("\n","");
+
+        System.out.println("Establishing connection");
         HubConnection hubConnection = HubConnectionBuilder.create(url).build();
+        System.out.println("Connected");
         hubConnection.start().blockingAwait();
-        hubConnection.send("OnConnected", "Autorization Basic admin:pj2017");
+        System.out.println("Authenticating");
+        hubConnection.send("OnConnected", authParams);
+        System.out.println("Success authentication");
 
     }
 }
