467db40b2f8fbc1f90c8322a8a1aa51eae34e2b4
Gendrys Espinosa Vega <gendrys.espinosa14@gmail.com>
2019-03-04 12:06:07 -0500

    Send parms autentication as headers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import com.microsoft.signalr.HubConnection;
 import com.microsoft.signalr.HubConnectionBuilder;
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.functions.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Base64;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         String url = "http://localhost:8088/sodexor";
         String user = "usr";
         String password = "password";
@@ -17,16 +24,14 @@ public class Main {
 
         String cred = user + ":" + password;
 
-        String resp = "Basic " + Base64.getEncoder().encode( cred.getBytes() );
-        String authParams = resp.replace("\n","");
+        String resp = "Basic " + Base64.getEncoder().encode(cred.getBytes());
+        String authParams = resp.replace("\n", "");
 
         System.out.println("Establishing connection");
-        HubConnection hubConnection = HubConnectionBuilder.create(url).build();
+        HubConnection hubConnection = HubConnectionBuilder.create(url)
+                .withHeader("Authorization",authParams).build();
         System.out.println("Connected");
         hubConnection.start().blockingAwait();
-        System.out.println("Authenticating");
-        hubConnection.send("OnConnected", authParams);
-        System.out.println("Success authentication");
 
     }
 }
